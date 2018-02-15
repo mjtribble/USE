19a0626ac814da0d0546c7df7a9a0862da8200db
Courtney Linder <courtneylinder@courtneys-mbp-2.home>
2018-02-15 11:38:10 -0700

    added a cmd to create instance of class and another to test some functions

---
+++ esof_homework3/question1/test_set1.cmd
@@ -0,0 +1,9 @@
+--this file call functions to see if transitions correctly
+!p1.seeOpponent()
+!p1.lostOpponent()
+!p1.seeOpponent()
+!p1.receiveBlow(50)
+!p1.receiveBlow(50)
+!p1.seeOpponent()
+!p1.lostOpponent()
+!p1.receiveBlow(10)
