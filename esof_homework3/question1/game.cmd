7af0b4167c31775d4cbf0ec5ca54cfc9180d8c1f
Courtney Linder <courtneylinder@courtneys-mbp-2.home>
2018-02-24 16:07:19 -0700

    addded weapon class and functions for it in player

--- esof_homework3/question1/game.cmd
+++ esof_homework3/question1/game.cmd
@@ -2,12 +2,15 @@
 
 --create objects
 !create p1:Player
+!create w1:Weapon
+!set w1.type:=weaponType::sword
 
 --call initialize method
 !p1.initInstance()
 
 --Insert Associations
 !insert (p1,p1.neutralState) into stateOfPlayer
+!insert(p1, p1.weapon) into weaponOfPlayer
 --Do we insert all state into association?
 !insert (p1,p1.attackState) into stateOfPlayer
 !insert (p1,p1.dieState) into stateOfPlayer
